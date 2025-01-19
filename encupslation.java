2c03c741dfc5ea8a4e60a74fb24e06ce85103153
Avinahs Reddy <avinashreddy1315@gmail.com>
2025-01-19 17:45:10 -0500

    concept of encuplation

--- encupslation.java
+++ encupslation.java
@@ -1,10 +1,36 @@
 class Human{
+    private int age;
+    private String name;
+
+    public int getAge(){
+        return age;
+    };
+
+    public void setAge(int a){
+        age = a;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String na){
+        name = na;
+    }
+
     
 }
 
 
 public class encupslation {
     public static void main(String[] args) {
+
+        Human obj1 = new Human();
+        obj1.setAge(25);
+        obj1.setName("Reddy");
+
+        System.out.println(obj1.getAge() + " : " + obj1.getName());
+
         
     }
 }
