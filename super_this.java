5d9101611898f5d76add05c380ee9b2424818d6c
Avinahs Reddy <avinashreddy1315@gmail.com>
2025-01-31 11:48:58 -0500

    Learned about sper and this keyword

--- super_this.java
+++ super_this.java
@@ -1,9 +1,15 @@
 
-
+//Call the construtor of the super class
 class A{ // super class
 
     public A(){
+        this(5);
         System.out.println("in A");
+    }
+
+    public A(int n){
+      
+        System.out.println("in A int");
     }
    
 }
@@ -12,8 +18,13 @@ class A{ // super class
 class B extends A{ //sub-class
 
     public B(){
-        
+       super();
         System.out.println("in B");
+    }
+
+    public B(int n){
+        this();
+        System.err.println("in B int");
     }
 
     
@@ -23,7 +34,7 @@ class B extends A{ //sub-class
 public class super_this {
     public static void main(String[] args){
 
-        B obj = new B();
+        B obj = new B(5);
 
    
         
